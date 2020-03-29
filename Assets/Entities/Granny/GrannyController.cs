2d6ba4c1417db362fae7f302839061e5496107fa
mudlee <contact@mudlee.hu>
2020-03-29 20:54:50 +0200

    Initial commit

---
+++ Assets/Entities/Granny/GrannyController.cs
@@ -0,0 +1,144 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using SAP2D;
+
+public class GrannyController : MonoBehaviour
+{
+    public static string TAG = "Granny";
+    public static int GRANNY_COUNTER = 0;
+    public GameObject _moveDotPrefab;
+    public Sprite _selectedSprite;
+    public Sprite _idleSprite;
+
+    private static int DOT_COUNTER = 0;
+    private static float SPEED = 2.5f;
+    
+    private int _id;
+    private List<DotEntry> _dots = new List<DotEntry>();
+    private SAP2DAgent _sAP2DAgent;
+    private bool _onItsWay;
+    private bool _infected;
+    private Animator _animator;
+
+    void Start()
+    {
+        _id = GRANNY_COUNTER;
+        _sAP2DAgent = GetComponent<SAP2DAgent>();
+        _sAP2DAgent.MovementSpeed = SPEED;
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (_onItsWay && _sAP2DAgent.Target != null)
+        {
+            if(Vector3.Distance(transform.position, _sAP2DAgent.Target.position) < 1f)
+            {
+                var dotToBeRemoved = _dots[0];
+                _dots.RemoveAt(0);
+                _sAP2DAgent.CanSearch = false;
+                Move();
+                dotToBeRemoved.Dot.SetActive(false);
+            }
+        }
+    }
+
+    public bool IsInfected()
+    {
+        return _infected;
+    }
+
+    public int GetPathLength()
+    {
+        return _dots.Count;
+    }
+
+    public void BeginMove(int expectedShops)
+    {
+        if (_dots.Count == 0 || !ReadyToMove(expectedShops))
+        {
+            return;
+        }
+        Selected(false);
+        _animator.SetBool("Walking", true);
+        _onItsWay = true;
+        Debug.Log(string.Format("Granny-{0} started moving as infected: {1}",_id,_infected));
+        Move();
+    }
+
+    public bool IsShopping()
+    {
+        return _onItsWay;
+    }
+
+    public bool ReadyToMove(int expectedShops)
+    {
+        return expectedShops == _dots.Select(d => d.ShopId).Distinct().Where(id => id != null).ToList().Count;
+    }
+
+    public void QueueDot(Vector2 at, int? shopId)
+    {
+        var dot = Instantiate(_moveDotPrefab);
+        dot.transform.position = at;
+        dot.name = string.Format("Granny-{0}-path-{1}", _id, ++DOT_COUNTER);
+        _dots.Add(new DotEntry(
+            dot.transform,
+            dot,
+            shopId
+        ));
+    }
+
+    public void RemoveLastPlacedDot()
+    {
+        var dotEntry = _dots[_dots.Count - 1];
+        Destroy(dotEntry.Dot);
+        _dots.RemoveAt(_dots.Count - 1);
+        --DOT_COUNTER;
+    }
+
+    public void Selected(bool selected)
+    {
+        _animator.SetBool("Selected", selected);
+    }
+
+    public void Infect()
+    {
+        _infected = true;
+        EventManager.TriggerEvent(EventManager.Event.GRANNY_INFECTED);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(_infected && collision.CompareTag(TAG))
+        {
+            collision.GetComponent<GrannyController>().Infect();
+        }
+    }
+
+    private void Move()
+    {
+        if (_dots.Count == 0)
+        {
+            _onItsWay = false;
+            _animator.SetBool("Walking", false);
+            return;
+        }
+        _sAP2DAgent.CanSearch = true;
+        _sAP2DAgent.Target = _dots[0].Position;
+    }
+
+    private class DotEntry
+    {
+        public Transform Position { get; }
+        public GameObject Dot { get; }
+        public int? ShopId { get; }
+
+        public DotEntry(Transform position, GameObject dot, int? shopId)
+        {
+            this.Position = position;
+            this.Dot = dot;
+            this.ShopId = shopId;
+        }
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,204 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject _grannyPrefab;
+    public GameObject[] _shops;
+    public GameObject _grannyInfo;
+    public GameObject _movePointsText;
+    public GameObject _readyText;
+    public GameObject _notReadyText;
+    public GameObject _startShoppingButton;
+    public GameObject _infectionRateText;
+    public GameObject _tip;
+    private GameObject _selectedGranny;
+    private List<GameObject> _grannies = new List<GameObject>();
+    private int _patientZero;
+    private bool _shopping;
+
+    void Start()
+    {
+        GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag("House");
+        _patientZero = Random.Range(0, spawnPoints.Length);
+
+        for (var i = 0; i < spawnPoints.Length; i++)
+        {
+            var granny = Instantiate(_grannyPrefab);
+            granny.transform.position = new Vector3(
+                spawnPoints[i].transform.position.x,
+                spawnPoints[i].transform.position.y - 1,
+                spawnPoints[i].transform.position.z
+            );
+            granny.name = string.Format("Granny-{0}", GrannyController.GRANNY_COUNTER++);
+            if (i == _patientZero)
+            {
+                granny.GetComponent<GrannyController>().Infect();
+            }
+
+            _grannies.Add(granny);
+        }
+
+        EventManager.StartListening(EventManager.Event.GRANNY_INFECTED, () => {
+            UpdateInfectionRate();
+        });
+        UpdateInfectionRate();
+
+        Debug.Log(string.Format("Patient Zero is {0}", _patientZero));
+        Debug.Log(string.Format("Number of Shops: {0}", _shops.Length));
+        Debug.Log(string.Format("Number of Grannies: {0}", _grannies.Count));
+    }
+
+    void Update()
+    {
+        ListenMouseClick();
+        ListenInputs();
+        WachResult();
+    }
+
+    public void RemoveTip()
+    {
+        Destroy(_tip);
+    }
+
+    public void StartShopping()
+    {
+        _startShoppingButton.SetActive(false);
+        _grannies.ForEach(g => g.GetComponent<GrannyController>().BeginMove(_shops.Length));
+        _shopping = true;
+    }
+
+    private void WachResult()
+    {
+        if(!_shopping)
+        {
+            return;
+        }
+
+        var numOfShopping = _grannies.Where(g => g.GetComponent<GrannyController>().IsShopping()).ToList().Count;
+        if(numOfShopping > 0)
+        {
+            return;
+        }
+
+        SceneManager.LoadScene(CalculateInfectionRate() > 60f ? "GameOver": "Won");
+    }
+
+    private void UpdateInfectionRate()
+    {
+        
+        _infectionRateText.GetComponent<TextMeshProUGUI>().SetText
+        (
+            "INFECTION RATE: " + CalculateInfectionRate().ToString() + "%"
+        );
+    }
+
+    private double CalculateInfectionRate()
+    {
+        var numberOfInfected = _grannies.Where(g => g.GetComponent<GrannyController>().IsInfected()).ToList().Count;
+        return System.Math.Round(((float)numberOfInfected / _grannies.Count * 100f), 1);
+    }
+
+    private void ListenMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            var hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            var grannyClicked = hit.collider != null && hit.collider.CompareTag(GrannyController.TAG);
+            var roadClicked = hit.collider != null && hit.collider.CompareTag("Road");
+            var shopClicked = hit.collider != null && hit.collider.CompareTag("Shop");
+            var grannySelected = _selectedGranny != null;
+
+            if (roadClicked && grannySelected)
+            {
+                Debug.Log("Road clicked with a selected granny");
+                var granny = _selectedGranny.GetComponent<GrannyController>();
+                var remainingMoves = GetMaxMovementPoints() - granny.GetPathLength();
+                if (remainingMoves == 0)
+                {
+                    return;
+                }
+                granny.QueueDot(mousePos2D, null);
+                UpdateGrannyInfo(granny);
+            }
+            else if(shopClicked && grannySelected)
+            {
+                Debug.Log("Shop clicked with a selected granny");
+                var granny = _selectedGranny.GetComponent<GrannyController>();
+                var remainingMoves = GetMaxMovementPoints() - granny.GetPathLength();
+                if (remainingMoves == 0)
+                {
+                    return;
+                }
+                granny.QueueDot(mousePos2D, hit.collider.GetHashCode());
+                UpdateGrannyInfo(granny);
+            }
+            else if(grannyClicked)
+            {
+                Debug.Log("Granny clicked");
+                var newGrannySelected = hit.collider.GetComponent<GrannyController>();
+                if (grannySelected)
+                {
+                    _selectedGranny.GetComponent<GrannyController>().Selected(false);
+                }
+
+                newGrannySelected.Selected(true);
+                _selectedGranny = hit.collider.gameObject;
+                _grannyInfo.SetActive(true);
+
+                UpdateGrannyInfo(newGrannySelected);
+            }
+            else if(grannySelected)
+            {
+                _grannyInfo.SetActive(false);
+                _selectedGranny.GetComponent<GrannyController>().Selected(false);
+                _selectedGranny = null;
+            }
+        }
+    }
+
+    private void ListenInputs()
+    {
+        if(Input.GetKeyDown(KeyCode.D) && _selectedGranny!=null)
+        {
+            var granny = _selectedGranny.GetComponent<GrannyController>();
+            if(granny.GetPathLength() == 0)
+            {
+                return;
+            }
+
+            granny.RemoveLastPlacedDot();
+            var remainingMoves = GetMaxMovementPoints() - granny.GetPathLength();
+            _movePointsText.GetComponent<TextMeshProUGUI>().SetText("REMAINING MOVES - " + remainingMoves);
+            UpdateGrannyInfo(granny);
+        }
+    }
+
+    private int GetMaxMovementPoints()
+    {
+        return _shops.Length * 2 + 2;
+    }
+
+    private void UpdateGrannyInfo(GrannyController granny)
+    {
+        var remainingMoves = GetMaxMovementPoints() - granny.GetPathLength();
+        _movePointsText.GetComponent<TextMeshProUGUI>().SetText("REMAINING MOVES - " + remainingMoves);
+
+        var grannyReady = granny.ReadyToMove(_shops.Length);
+        _readyText.SetActive(grannyReady);
+        _notReadyText.SetActive(!grannyReady);
+
+        CheckIfAllGranniesAreReady();
+    }
+
+    private void CheckIfAllGranniesAreReady()
+    {
+        var numOfReady = _grannies.Where(g => g.GetComponent<GrannyController>().ReadyToMove(_shops.Length)).ToList().Count;
+        _startShoppingButton.SetActive(numOfReady == _grannies.Count);
+    }
+}
